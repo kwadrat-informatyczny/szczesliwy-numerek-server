eba1788cfc5f4adf92ffec94d3ccac941ec53fbe
RemekZ <33695253+RemekZ@users.noreply.github.com>
2018-02-01 22:42:09 +0100

    Add files via upload

---
+++ next_date.js
@@ -0,0 +1,14 @@
+var jutro = new Date();
+function getNextDrawTime() {
+	if (jutro.getDay() == 5){
+		jutro.setDate(jutro.getDate() + 2);
+	}	 else if (jutro.getDay() == 4 && jutro.getHours() >= 20 && jutro.getMinutes() >= 00){
+		jutro.setDate(jutro.getDate() + 3);
+	} else if (jutro.getHours() < 20){
+				jutro = new Date();
+	} else {
+	jutro.setDate(jutro.getDate() + 1);
+}jutro.setHours(20);
+jutro.setMinutes(0);
+jutro.setSeconds(0);
+}
