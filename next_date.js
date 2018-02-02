b5c0de7d6a6c2484ad251d2924d16051243d5d33
michal-bielinski <33670563+michal-bielinski@users.noreply.github.com>
2018-02-02 16:47:48 +0100

    Update next_date.js

--- next_date.js
+++ next_date.js
@@ -1,14 +1,21 @@
-var jutro = new Date();
-function getNextDrawTime() {
+function getNextDrawTime(czas) {
+  var jutro = new Date(czas.getTime());
+  
 	if (jutro.getDay() == 5){
 		jutro.setDate(jutro.getDate() + 2);
-	}	 else if (jutro.getDay() == 4 && jutro.getHours() >= 20){
+  }
+  else if (jutro.getDay() == 6) {
+		jutro.setDate(jutro.getDate() + 1);
+  } else if (jutro.getDay() == 4 && jutro.getHours() >= 20){
 		jutro.setDate(jutro.getDate() + 3);
 	} else if (jutro.getHours() < 20){
 				jutro = new Date();
 	} else {
-	jutro.setDate(jutro.getDate() + 1);
-}jutro.setHours(20);
-jutro.setMinutes(0);
-jutro.setSeconds(0);
+	  jutro.setDate(jutro.getDate() + 1);
+  }
+  jutro.setHours(20);
+  jutro.setMinutes(0);
+  jutro.setSeconds(0);
+
+  return jutro;
 }
