e1771ab1049d84f8b33c390ccf33a5dca65a73bb
michal-bielinski <33670563+michal-bielinski@users.noreply.github.com>
2018-03-13 14:48:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var dane = {},
 dane['school_name'] = "ZS1 Bochnia";
 dane['school_ico'] = "http://zs1.bochnia.pl/images/logobiale.png";
 
-console.log("------" + dane.size)
 fs.readFile('settings.json',  "utf8", (err, data) => {
   if (err) console.log("Problem with reading file! " + err.toString())
   console.log('Settings file: ' + data);
@@ -20,41 +19,42 @@ fs.readFile('settings.json',  "utf8", (err, data) => {
   if (!dane.toDraw || dane.toDraw.length == 0) {
     let c = 0;
     dane.toDraw = [];
-    while ( c < dane.size) {
+    while (c < dane.size) {
       c++;
       dane.toDraw.push(c);
     }
   }
   console.log("List of numbers to be used in generation: " + dane.toDraw.toString())
 
-  // check if we need to generate number
-  var now = new Date();
-  var when = getNextDrawTime(new Date());
-
-  console.log("Current time: " + now.toISOString() + ", next draw: " + when.toISOString())
-
-  if ((when.getTime() <= now.getTime()) || !dane.numerek) {
-    dane.numerek = getRandomInt(0, dane.toDraw.length);
-    dane.toDraw.splice(dane.toDraw.indexOf(dane.numerek), 1);
-    dane['data_losowania'] = now.toISOString();
-
-    console.log("GENERATED NUMBER --> " + dane.numerek + " <--")
-  }
-  console.log("List of numbers to be used in future generation: " + dane.toDraw.toString())
-
   fs.writeFile('settings.json', JSON.stringify(dane) , (err) => {
     if (err) return console.log("Problem with saving file! " + err.toString())
   });
 });
 
-/*dane.numerek = numerek;
-dane['data_losowania'] = new Date().getTime()
-var jutro = new Date();
-jutro.setDate(jutro.getDate() + 1);
-dane['data_kolejnego_losowania'] = jutro.getTime()*/
-
-
 http.createServer(function(request, response){
+
+  // check if we need to generate number
+  var now = new Date(),
+    old = new Date(now.getTime() - 24 * 60 * 60);
+
+  if (dane['data_losowania'])
+    old = new Date(dane['data_losowania']);
+
+  var when = getNextDrawTime(old);
+  console.log("Last draw time: " + old.toISOString() + ", next draw: " + when.toISOString())
+
+  if ((when.getTime() <= now.getTime()) || !dane.numerek) {
+    dane.numerek = getRandomInt(0, dane.toDraw.length);
+    dane.toDraw.splice(dane.toDraw.indexOf(dane.numerek), 1);
+    dane['data_losowania'] = now.getTime();
+
+    console.log("GENERATED NUMBER --> " + dane.numerek + " <--")
+
+    fs.writeFile('settings.json', JSON.stringify(dane) , (err) => {
+      if (err) return console.log("Problem with saving file! " + err.toString())
+    });
+  }
+
   response.writeHead(200, {'Content-type':'application/json'});
   response.write( JSON.stringify(dane) );
   response.end( );
